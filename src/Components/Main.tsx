829168c6ae9086224acfe3827261ed98827a5f64
Razzaq <66423346+lectr1c@users.noreply.github.com>
2024-01-22 16:11:25 +0100

    added About me section responsive
    fixed overflow

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,5 +1,18 @@
 import "../Styles/Main.scss";
+import image from "../assets/Presenting.jpg"
 
 export default function Main() {
-  return <div className="main"><h1>PORTFOLIO DEVELOPMENT IN PROGRESS</h1></div>;
+  return (<>
+    <section className="presentingSection">
+      <div>
+        <h1>About me</h1>
+        <p>Suspendisse tincidunt sit amet magna non vulputate. Aliquam sed libero non erat commodo lobortis. Duis venenatis posuere lacus eget tristique. Donec neque felis, malesuada ut sollicitudin sed, varius non elit. Phasellus ultrices augue metus. Mauris est nisi, luctus sed tellus sit amet, ullamcorper dapibus ligula. In consequat lacus at nibh pellentesque congue.  </p>
+      </div>
+    </section>
+    <div className="main">
+      <div>
+        W.I.P. last updated 01/22/2023
+      </div>
+    </div>
+  </>)
 }
