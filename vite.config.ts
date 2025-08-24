90ca4ca65fc88ee23323a6d36aab5fa31b5fc31f
Abdulrazzaq Summaq <razzaq@Abdulrazzaqs-MacBook-Pro.local>
2025-08-24 20:46:05 +0200

    portfolio redesign build

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/docs/', // 👈 This is what's missing
+    base: '/portfolio/', // 👈 This is what's missing
     plugins: [react()],
     build: {
         outDir: './docs'
